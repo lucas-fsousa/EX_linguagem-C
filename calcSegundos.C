3ec6f96ddaa9ef0953254f2f191631a9cc9f8351
Lucas de Sousa <66752018+lucas-fsousa@users.noreply.github.com>
2020-09-02 22:44:10 -0300

    Add files via upload

---
+++ calcSegundos.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    // Declaração de variavel
+    int s, m, h;
+    
+    // Entrada de dados
+    printf("Digite a quantidade de segundos: ");
+    scanf("%d", &s);
+    
+    // Cálculo de horas, minutos e segundos
+    m = s / 60;
+    h = m / 60;
+    
+    // Ajuste De horas minutos e segundos para o Padrão
+    m = m % 60;
+    h = h % 60;
+    s = s % 60;
+    
+    // Informe do resultado final para o usuário
+    printf("%d horas > %d minutos > %d segundos", h, m, s);
+    
+    // Retorno de erros caso ocorram
+    return 0;
+}
